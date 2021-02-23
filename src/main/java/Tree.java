35a4c52208fa10a4eac3c6efd3c1649a74ab3795
ilyaorlov25 <ilya.orlov2002@yandex.ru>
2021-02-24 02:16:35 +0300

    move to 12 level & almost remove

--- src/main/java/Tree.java
+++ src/main/java/Tree.java
@@ -43,66 +43,113 @@ public class Tree<T extends Comparable<T>> implements Set<T> {
         return node;
     }
 
-    private Node<T> insert(Node<T> root, T key) {
-        // Вставить ключ k в дерево с корнем root
-        if (root == null) return new Node<T>(key);
+    // Вставить ключ key в поддерево с корнем в узле node
+    private Node<T> insert(Node<T> node, T key) {
+        if (node == null) return new Node<T>(key);
 
-        int comparison = key.compareTo(root.key);
+        int comparison = key.compareTo(node.key);
         if (comparison < 0)
-            root.left = insert(root.left, key);
+            node.left = insert(node.left, key);
         else
-            root.right = insert(root.right, key);
-        return balance(root);
+            node.right = insert(node.right, key);
+        return balance(node);
     }
 
+    // Поиск минимального ключа в поддереве с корнем в узле node
+    private Node<T> findMin(Node<T> node) {
+        return (node.left == null)? node : findMin(node.left);
+    }
+
+    // Удаление узла с минимальным ключом из поддерева с корнем в узле node
+    private Node<T> removeMin(Node<T> node) {
+        if (node.left == null) return node.right;
+        node.left = removeMin(node.left);
+        return balance(node);
+    }
+
+    // Удаление узла с ключом key из поддерева с корнем node
+    private Node<T> remove(Node<T> node, T key) {
+        if (node == null) return null;
+
+        int comparison = key.compareTo(node.key);
+        if (comparison < 0) {
+            node.left = remove(node.left, key);
+        } else if (comparison > 0) {
+            node.right = remove(node.right, key);
+        } else {
+            Node<T> left = node.left;
+            Node<T> right = node.right;
+            // ДОДЕЛАТЬ
+        }
+        // ДОДЕЛАТЬ
+        return null;
+    }
+
+    private Node<T> find(T key) {
+        return null;
+    }
+
+    @Override
     public int size() {
         return 0;
     }
 
+    @Override
     public boolean isEmpty() {
         return false;
     }
 
+    @Override
     public boolean contains(Object o) {
         return false;
     }
 
+    @Override
     public Iterator<T> iterator() {
         return null;
     }
 
+    @Override
     public Object[] toArray() {
         return new Object[0];
     }
 
+    @Override
     public <T1> T1[] toArray(T1[] a) {
         return null;
     }
 
+    @Override
     public boolean add(T t) {
         return false;
     }
 
+    @Override
     public boolean remove(Object o) {
         return false;
     }
 
+    @Override
     public boolean containsAll(Collection<?> c) {
         return false;
     }
 
+    @Override
     public boolean addAll(Collection<? extends T> c) {
         return false;
     }
 
+    @Override
     public boolean retainAll(Collection<?> c) {
         return false;
     }
 
+    @Override
     public boolean removeAll(Collection<?> c) {
         return false;
     }
 
+    @Override
     public void clear() {
 
     }
